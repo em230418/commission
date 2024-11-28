bbb47286cb7badfdbbfc41ef78f52f75415e6df5
Eugene Molotov <molotov@it-projects.info>
2024-11-28 14:54:08 +0500

    Перенесли миграционные скриптты в правильное место

--- account_commission/migrations/15.0.2.0.0/post-migration.py
+++ account_commission/migrations/15.0.2.0.0/post-migration.py
@@ -5,6 +5,31 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
+    """Convert the former `agent_line` m2m relation in `commission.line.mixin` into
+    the new `settlement_line_ids` o2m relation."""
+    openupgrade.logged_query(
+        env.cr,
+        """
+            UPDATE commission_settlement_line
+            SET invoice_agent_line_id = sal_rel.agent_line_id
+            FROM (
+                SELECT DISTINCT ON (agent_line_id) agent_line_id, settlement_id
+                FROM settlement_agent_line_rel
+                ORDER BY agent_line_id
+            ) AS sal_rel
+            WHERE id = sal_rel.settlement_id
+        """,
+    )
+
+    # All the existing settlements are of this type for now
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE commission_settlement
+        SET settlement_type = 'sale_invoice'
+        """,
+    )
+
     """Fill new invoice_agent_line_id many2one from the old many2many agent_line
     equivalent table. This table always contain only one record on standard use.
     """
